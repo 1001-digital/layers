2f126d3f4e9e8272ac9ec6796ee39694fd8bb1ef
jalil <jalil@1001.digital>
2025-12-15 13:11:54 +0100

    Set up modern normalize

--- base/nuxt.config.ts
+++ base/nuxt.config.ts
@@ -1,4 +1,13 @@
+import { fileURLToPath } from 'node:url'
+import { dirname, join } from 'node:path'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
+  css: [
+    join(currentDir, './app/assets/styles/index.css'),
+  ],
 })
