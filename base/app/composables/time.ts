9c752733f5a289583c46d6859f7e8c420357820b
jalil <jalil@1001.digital>
2026-01-14 18:21:52 +0100

    Add time helpers

---
+++ base/app/composables/time.ts
@@ -0,0 +1,63 @@
+import { DateTime } from 'luxon'
+import type { Ref } from 'vue'
+
+let nowInterval: NodeJS.Timeout | undefined
+
+export const useSeconds = () => {
+  const now = useState<number>('now', () => nowInSeconds())
+
+  if (import.meta.client && !nowInterval) {
+    nowInterval = setInterval(() => {
+      now.value = nowInSeconds()
+    }, 1000)
+  }
+
+  return now
+}
+
+export const useCountDown = (s: Ref<number | bigint>, showSecondsWithin: number = 60) => {
+  const duration = computed(() => Math.abs(Number(s.value)))
+
+  const seconds = computed(() => duration.value % 60)
+  const minutes = computed(() => Math.floor(duration.value / 60) % 60)
+  const hours = computed(() => Math.floor(duration.value / 60 / 60) % 24)
+  const days = computed(() => Math.floor(duration.value / 60 / 60 / 24))
+
+  const str = computed(() =>
+    [
+      days.value ? `${days.value}d` : null,
+      hours.value ? `${hours.value}h` : null,
+      minutes.value ? `${minutes.value}m` : null,
+      duration.value < showSecondsWithin && seconds.value ? `${seconds.value}s` : null,
+    ]
+      .filter((s) => !!s)
+      .join(' '),
+  )
+
+  return {
+    seconds,
+    minutes,
+    hours,
+    days,
+    str,
+  }
+}
+
+export const useSecondsAgo = (time: Ref<string | undefined>) => {
+  const ago = ref<string>()
+  const now = useSeconds()
+
+  watch(
+    now,
+    () => {
+      if (time.value) {
+        ago.value = DateTime.fromISO(time.value).toRelative({ style: 'short', locale: 'en' }) ?? undefined
+      }
+    },
+    {
+      immediate: true,
+    },
+  )
+
+  return ago
+}
---
+++ base/app/utils/time.ts
@@ -0,0 +1,20 @@
+import { DateTime } from 'luxon'
+
+export const delay = (ms: number): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve, ms))
+
+export const daysInSeconds = (days: number): number => days * 60 * 60 * 24
+
+export const nowInSeconds = (): number => Math.floor(Date.now() / 1000)
+
+export const asUTCDate = (date: Date | null) =>
+  date
+    ? DateTime.utc(
+      date.getFullYear(),
+      date.getMonth() + 1,
+      date.getDate(),
+      date.getHours(),
+      date.getMinutes(),
+      date.getSeconds(),
+    )
+    : null
