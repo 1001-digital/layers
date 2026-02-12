e825010c05845748a925a9057369ea2805364806
jalil <jalil@1001.digital>
2026-02-12 23:56:39 +0100

    Add missing icon mappings

--- base/nuxt.config.ts
+++ base/nuxt.config.ts
@@ -12,11 +12,15 @@ export default defineNuxtConfig({
   icon: {
     componentName: 'NuxtIcon',
     aliases: {
+      add: 'lucide:plus',
       check: 'lucide:check',
       'chevron-down': 'lucide:chevron-down',
       close: 'lucide:x',
       copy: 'lucide:copy',
+      edit: 'lucide:pencil',
       help: 'lucide:circle-question-mark',
+      home: 'lucide:house',
+      link: 'lucide:link',
       loader: 'lucide:loader-2',
     }
   },
