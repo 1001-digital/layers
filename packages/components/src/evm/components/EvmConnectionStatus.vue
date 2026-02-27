e7576819c9a0e90265b01ccd66e6b8206c959b24
jalil <jalil@1001.digital>
2026-02-27 21:48:28 +0100

    Implement generic connection status component

---
+++ packages/components/src/evm/components/EvmConnectionStatus.vue
@@ -0,0 +1,26 @@
+<template>
+  <slot
+    v-if="status === 'connected'"
+    name="connected"
+    :address="address!"
+    :connector="connector!"
+  />
+  <slot
+    v-else-if="status === 'reconnecting'"
+    name="reconnecting"
+  />
+  <slot
+    v-else-if="status === 'connecting'"
+    name="connecting"
+  />
+  <slot
+    v-else
+    name="disconnected"
+  />
+</template>
+
+<script setup lang="ts">
+import { useConnection } from '@wagmi/vue'
+
+const { address, connector, status } = useConnection()
+</script>
